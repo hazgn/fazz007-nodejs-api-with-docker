d997d582345054298eef54e1c356360bebf119db
Hazpi Nurafgan <hazvinurafgan@gmail.com>
2023-03-04 08:39:37 +0700

    feat: fature/update

--- src/models/post.model.js
+++ src/models/post.model.js
@@ -1,38 +1,61 @@
-const db = require('../configs/db')
-const models = {}
+const db = require("../configs/db");
+const models = {};
 
 models.all = () => {
   return new Promise((resolve, reject) => {
     db.query(`SELECT * FROM tbl_posts`, (err, result, fields) => {
-      if(err){
-        reject(err)
-      }else{
-        resolve(result)
+      if (err) {
+        reject(err);
+      } else {
+        resolve(result);
       }
-    })
-  })
-}
+    });
+  });
+};
 models.detail = (id) => {
   return new Promise((resolve, reject) => {
-    db.query(`SELECT * FROM tbl_posts WHERE id=?`, [id], (err, result, fields) => {
-      if(err){
-        reject(err)
-      }else{
-        resolve(result)
+    db.query(
+      `SELECT * FROM tbl_posts WHERE id=?`,
+      [id],
+      (err, result, fields) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
       }
-    })
-  })
-}
-models.store = ({title, description}) => {
+    );
+  });
+};
+models.store = ({ title, description }) => {
   return new Promise((resolve, reject) => {
-    db.query(`INSERT INTO tbl_posts (title, description) VALUES (?,?)`, [title, description], (err, result, fields) => {
-      if(err){
-        reject(err)
-      }else{
-        resolve(result)
+    db.query(
+      `INSERT INTO tbl_posts (title, description) VALUES (?,?)`,
+      [title, description],
+      (err, result, fields) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
       }
-    })
-  })
-}
+    );
+  });
+};
+models.update = ({ id, title, description }) => {
+  return new Promise((resolve, reject) => {
+    db.query(
+      `UPDATE tbl_posts SET title = ?, description = ? WHERE id = ?`,
+      [title, description, id],
+      (err, result, fields) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      }
+    );
+  });
+};
 
-module.exports = models
+module.exports = models;
